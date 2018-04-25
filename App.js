464f761037ab6887ccb2bd308414b22f6780ae23
zhangtonglei <zhangtonglei@tidebuy.net>
2018-04-25 19:02:27 +0800

    react-nativation

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ import {
   View
 } from 'react-native';
 
-import Router from './app/router/Draw';
+import Router from './app/router/Router';
 type Props = {};
 export default class App extends Component<Props> {
   render() {
--- app/router/Draw.js
+++ app/router/Draw.js
@@ -16,6 +16,7 @@ const RootDrawer = DrawerNavigator({
         screen: Tab,
     },
 
+
 });
 
 export default RootDrawer;

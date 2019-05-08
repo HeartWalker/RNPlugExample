8445f0ed7a7c908486fa67ffac0cbcba6cd58479
zhtlei <>
2019-05-08 15:56:22 +0800

    rn upgrade to 0.59.5

--- app/Pre.js
+++ app/Pre.js
@@ -2,46 +2,38 @@
  * 预处理
  */
 
-import React, {Component, cloneElement} from 'react';
-import {
-    Text,
-    Alert,
-} from 'react-native';
+import React, { Component, cloneElement } from "react";
+import { Text, Alert } from "react-native";
 
-
-
-
-
-let styles = {color:'#333'}
-let render = Text.prototype.render;
-Text.prototype.render = function (...args) {
-    let originText = render.apply(this, args);
-    return cloneElement(originText, {
-        style: [
-            styles,
-            originText.props.style,
-        ]
-    });
+let styles = { color: "#333" };
+// let render = Text.prototype.render;
+// Text.prototype.render = function (...args) {
+//     let originText = render.apply(this, args);
+//     return cloneElement(originText, {
+//         style: [
+//             styles,
+//             originText.props.style,
+//         ]
+//     });
+// };
+let oldRender = Text.render;
+Text.render = function(...args) {
+  let origin = oldRender.call(this, ...args);
+  return React.cloneElement(origin, {
+    style: [styles, origin.props.style]
+  });
 };
 
-
 if (!__DEV__) {
-    global.console = {
-        info: () => {
-        },
-        log: () => {
-        },
-        warn: () => {
-        },
-        debug: () => {
-        },
-        error: () => {
-        },
-    };
+  global.console = {
+    info: () => {},
+    log: () => {},
+    warn: () => {},
+    debug: () => {},
+    error: () => {}
+  };
 
-
-     ErrorUtils.setGlobalHandler((e) => {
-     //Alert.alert("异常", JSON.stringify(e));
-     });
+  ErrorUtils.setGlobalHandler(e => {
+    //Alert.alert("异常", JSON.stringify(e));
+  });
 }
-
